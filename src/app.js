73df295972b2a84f1b8747452ddb3f18c992897f
rahulsharmamsg <rahulsharma1071993@gmail.com>
2025-05-15 16:03:14 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from "express"
 const app = express();
 
 app.get('/',(req,res,next)=>{
-    res.send("CICD completed!")
+    res.send("Hey, CICD completed!")
 })
 app.listen("4000",()=>{
     console.log("Server start port on 4000" )
